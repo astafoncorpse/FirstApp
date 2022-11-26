3317d591aee72d94ca5d19f7061ab3e109f32f98
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2022-11-26 20:26:51 +0300

    ура я инициализировал переменную. модуль 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string MyName = "Roma";
+            Console.WriteLine(MyName);
+            Console.WriteLine("\t Привет, \nмир!");
+            Console.WriteLine("\t Мне 28 лет");
+            Console.WriteLine("\t My name is \n Roma");
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23');
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(5);
+            
+            Console.ReadKey();
         }
     }
 }
